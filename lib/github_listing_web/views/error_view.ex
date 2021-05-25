d7c5d64ea90c617f41262bd59536fb6e5042e09d
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-25 05:30:22 -0300

    render now set

--- lib/github_listing_web/views/error_view.ex
+++ lib/github_listing_web/views/error_view.ex
@@ -1,6 +1,10 @@
 defmodule GithubListingWeb.ErrorView do
   use GithubListingWeb, :view
 
+  # import Ecto.Changeset, only: [traverse_errors: 2]
+
+  # alias Ecto.Changeset
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.json", _assigns) do
@@ -13,4 +17,24 @@ defmodule GithubListingWeb.ErrorView do
   def template_not_found(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
+
+  # def render("error.json", %{result: result}) do
+  #   %{
+  #     message: result
+  #   }
+  # end
+
+  def render("error.json", %{result: result}) do
+    %{
+      message: result
+    }
+  end
+
+  # defp translate_errors(changeset) do
+  #   traverse_errors(changeset, fn {msg, opts} ->
+  #     Enum.reduce(opts, msg, fn {key, value}, acc ->
+  #       String.replace(acc, "%{#{key}}", to_string(value))
+  #     end)
+  #   end)
+  # end
 end
