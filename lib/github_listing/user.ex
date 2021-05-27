13f93345f1b39394553d2a1402d71d1132a77973
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-27 06:24:40 -0300

    changeset for user creation is set but with no auth

---
+++ lib/github_listing/user.ex
@@ -0,0 +1,37 @@
+defmodule GithubListing.User do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:email, :password]
+
+  @derive {Jason.Encoder, only: [:id, :email]}
+
+  schema "users" do
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 6)
+    |> unique_constraint([:email])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
