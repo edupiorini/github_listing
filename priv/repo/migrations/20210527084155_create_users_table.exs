d7c9db8024f8b04f6d1c12e097eeb68d33ab4b44
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-27 05:45:39 -0300

    users table migration ok

---
+++ priv/repo/migrations/20210527084155_create_users_table.exs
@@ -0,0 +1,13 @@
+defmodule GithubListing.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:user) do
+      add :email, :string
+      add :password_hash, :string
+
+      timestamps()
+    end
+
+  end
+end
