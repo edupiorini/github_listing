e56c0863f10bfcb298123c07d62ac8cd40da83e7
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-31 07:48:04 -0300

    traverse errors set on err view

--- lib/github_listing_web/views/error_view.ex
+++ lib/github_listing_web/views/error_view.ex
@@ -1,5 +1,8 @@
 defmodule GithubListingWeb.ErrorView do
   use GithubListingWeb, :view
+  import Ecto.Changeset, only: [traverse_errors: 2]
+
+  alias Ecto.Changeset
 
   # import Ecto.Changeset, only: [traverse_errors: 2]
 
@@ -18,11 +21,11 @@ defmodule GithubListingWeb.ErrorView do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 
-  # def render("error.json", %{result: result}) do
-  #   %{
-  #     message: result
-  #   }
-  # end
+  def render("error.json", %{result: %Changeset{} = changeset}) do
+    %{
+      message: translate_errors(changeset)
+    }
+  end
 
   def render("error.json", %{result: result}) do
     %{
@@ -30,11 +33,11 @@ defmodule GithubListingWeb.ErrorView do
     }
   end
 
-  # defp translate_errors(changeset) do
-  #   traverse_errors(changeset, fn {msg, opts} ->
-  #     Enum.reduce(opts, msg, fn {key, value}, acc ->
-  #       String.replace(acc, "%{#{key}}", to_string(value))
-  #     end)
-  #   end)
-  # end
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
 end
