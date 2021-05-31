6dff15311f9e328efc590e5a00c9a763c3e4d7c1
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-05-31 06:37:37 -0300

    facade upgraded

--- lib/github_listing.ex
+++ lib/github_listing.ex
@@ -6,8 +6,11 @@ defmodule GithubListing do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
-
   alias GithubListing.GithubApi.Client
+  alias GithubListing.Users.Create, as: UserCreate
+  alias GithubListing.Users.Get, as: UserGet
 
   defdelegate list_repositories(username), to: Client, as: :get_repo_list
+  defdelegate get_user(id), to: UserGet, as: :call
+  defdelegate create_user(params), to: UserCreate, as: :call
 end
